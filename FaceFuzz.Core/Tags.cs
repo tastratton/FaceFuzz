9d5ba4ab4e6b2ece71b1e808d290ba4bac395fa0
Thomas Stratton <tastratton@georgiasown.org>
2024-08-22 14:47:49 -0500

    beginning work on tags

---
+++ FaceFuzz.Core/Tags.cs
@@ -0,0 +1,21 @@
+﻿namespace FaceFuzz.Core;
+public class Tags
+{
+    public string StartTag { get; private set; }
+    public string EndTag { get; private set; }
+    public Tags()
+    {
+        // default tags
+        StartTag = "{{";
+        EndTag = "}}";
+    }
+
+    public Tags(string startTag, string endTag)
+    {
+       throw new NotImplementedException();
+        // todo: needs validation for rules such as: start and end tags cant be same thing, can't be empty, tags are n characters, and other rules
+       //StartTag = startTag;
+       //EndTag = endTag;
+
+    }
+}
